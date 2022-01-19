f2c2101d0be914f2bd56aa75de52a4c04a3d3dd0
Orel <Orely4242@gmail.com>
2022-01-19 22:46:05 +0200

    beyond2lte: adapt for PixelExperience 12

---
+++ aosp_beyond2lte.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+## Inherit from generic products, most specific first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+## Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+## Inherit from beyond2lte device
+$(call inherit-product, device/samsung/beyond2lte/device.mk)
+
+## Inherit some common build stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+## Boot Animation
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 3040
+TARGET_SCREEN_WIDTH := 1440
+TARGET_HAVE_FOD := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+## Device identifier, this must come after all inclusions
+PRODUCT_NAME := aosp_beyond2lte
+PRODUCT_DEVICE := beyond2lte
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy S10+
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
